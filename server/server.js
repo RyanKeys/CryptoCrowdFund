dc24064030fc4319c1d1f80ba7d94f7d20fd6f4d
Ryan Keys <ryankeys@Ryans-MacBook-Pro.local>
2021-03-16 20:22:46 -0700

    added Handlebars template engine

--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
 const path = require("path");
 const Web3 = require("web3");
 
 const app = express();
+app.use(express.static("client"));
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+const port = 3000;
+
 const rpcURL = "https://rinkeby.infura.io/v3/fd78f6f784a64fe1a20865081c28a049";
 const web3 = new Web3(rpcURL);
 const address = "0x60680e1c9de55a4e65653b4b56ea93791e7fd64f";
 
-app.use(express.static("client"));
-
-const port = 3000;
-const dirname = __dirname.replace("server", "client/");
-const clientPath = path.join(dirname + "index.html");
-
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/profile", (req, res) => {
   web3.eth.getBalance(address, (err, wei) => {
     balance = web3.utils.fromWei(wei, "ether");
-    res.send(balance);
+    res.render("profile", { balance: balance });
   });
 });
 
